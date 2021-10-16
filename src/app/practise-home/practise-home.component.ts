547c2ac8d8ad01a6d29fe376f8e973a17ea673ae
maknatar <maknatar@cisco.com>
2021-10-16 18:20:18 +0530

    removed swal

--- src/app/practise-home/practise-home.component.ts
+++ src/app/practise-home/practise-home.component.ts
@@ -1,74 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import swal from 'Sweetalert';
+// import swal from 'Sweetalert';
 import { Router } from '@angular/router';
 import { LocalStorage } from '@ngx-pwa/local-storage';
 @Component({
   selector: 'app-practise-home',
   templateUrl: './practise-home.component.html',
-  styleUrls: ['./practise-home.component.css']
+  styleUrls: ['./practise-home.component.css'],
 })
 export class PractiseHomeComponent implements OnInit {
-
-  constructor(private router: Router,private localStorage: LocalStorage) { }
-show_nav: boolean;
+  constructor(private router: Router, private localStorage: LocalStorage) {}
+  show_nav: boolean;
   ngOnInit(): void {
     this.show_nav = false;
   }
-  show(){
-    this.show_nav = !this.show_nav
+  show() {
+    this.show_nav = !this.show_nav;
   }
-logout(){
 
 
-  swal({
-    // buttons: {
-    //   cancel: true,
-    //   confirm: true,
-    // },
-    text: 'Are you sure ?',
+  logout(): void {
 
-    // icon: 'warning',
-// buttons: {
-//       cancel: true,
-//       confirm: true,
-//       // confirm: 'Yes, Logout !',
-//     },
-buttons: ["No", "Yes, Logout"],
+    let isLogOut = confirm("Are you sure you want to log out?");
 
-  }).then((result) => {
-
-
-    if (result) {
-
-      // this.logServ.logout();
+    if(isLogOut) {
       localStorage.clear();
       this.router.navigate(['/login']);
-
-
-      swal("Logout Success !", {
-
-        timer: 3000,
-
-      });
     }
-    // else if (result.dismiss === swal.DismissReason.cancel) {
-
-    // }
-
-  });
-//     console.log('BEFROR'+localStorage.length);
-// localStorage.clear();
-
-// console.log('Aftrr'+localStorage.length);
-// this.router.navigateByUrl('/login');
-}
-goHome(){
-  this.router.navigateByUrl('/home');
-}
-goProfile(){
-  this.router.navigateByUrl('/profile');
-}
-goSettings(){
-  this.router.navigateByUrl('/settings');
-}
+    // swal({
+    //   text: 'Are you sure ?',
+    //   buttons: ['No', 'Yes, Logout'],
+    // }).then((result) => {
+    //   if (result) {
+    //     localStorage.clear();
+    //     this.router.navigate(['/login']);
+    //   }
+    // });
+  }
+  goHome() {
+    this.router.navigateByUrl('/home');
+  }
+  goProfile() {
+    this.router.navigateByUrl('/profile');
+  }
+  goSettings() {
+    this.router.navigateByUrl('/settings');
+  }
 }
